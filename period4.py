c70f3381c8d24471109a2d4403aaf3131cb6a6b5
vinnyongit <144164943+vinnyongit@users.noreply.github.com>
2023-09-25 20:12:15 -0700

    Update period4.py

--- period4.py
+++ period4.py
@@ -1,26 +1,38 @@
-selections = ["Rock", "Paper", "Scissors"]
-player_selection = input("Would you like to select Rock, Paper, or Scissors?")
-import random
-ai_selection = random.choice(selections)
-print("Your opponent selected " + ai_selection + "!")
-if (ai_selection == "Rock"):
-    if (player_selection == "Paper"):
-        print("You won!") 
-    if (player_selection == "Scissors"):
-        print("You lost!")
-    if (player_selection == "Rock"):
-        print("Tie!")
-if (ai_selection == "Paper"):
-    if (player_selection == "Paper"):
-        print("Tie!") 
-    if (player_selection == "Scissors"):
-        print("You won!")
-    if (player_selection == "Rock"):
-        print("You lost!")
-if (ai_selection == "Scissors"):
-    if (player_selection == "Paper"):
-        print("You lost!") 
-    if (player_selection == "Scissors"):
-        print("Tie!")
-    if (player_selection == "Rock"):
-        print("You Won!")
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+def check_winner(board, player):
+    for row in board:
+        if all(cell == player for cell in row):
+            return True
+    for col in range(3):
+        if all(board[row][col] == player for row in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+def is_board_full(board):
+    return all(cell != " " for row in board for cell in row)
+def play_game():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+    print("Welcome to Tic-Tac-Toe!")
+    print_board(board)
+    while True:
+        row = int(input(f"Player {current_player}, enter the row (0, 1, 2): "))
+        col = int(input(f"Player {current_player}, enter the column (0, 1, 2): "))
+        if row < 0 or row > 2 or col < 0 or col > 2 or board[row][col] != " ":
+            print("Invalid move. Try again.")
+            continue
+        board[row][col] = current_player
+        print_board(board)
+        if check_winner(board, current_player):
+            print(f"Player {current_player} wins!")
+            break
+        elif is_board_full(board):
+            print("It's a draw!")
+            break
+        current_player = "O" if current_player == "X" else "X"
+if __name__ == "__main__":
+    play_game()
